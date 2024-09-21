fcec714539919a72109fbd1f53d584b841ad38c0
Ebrahim mamdouh <138860098+ebrahim-mamdoh@users.noreply.github.com>
2024-09-21 15:00:07 +0300

    our product

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Footer from "./components/Footer/Footer"
 import Header from './components/Header/Header';
 import PestSale from './components/PestSale/PestSale';
+import Sale from './components/Sale/Sale';
 
 function App() {
   return (
    <>
    <Header/>
    <PestSale/>
+   <Sale/>
    <Footer/>
    </>
   );
---
+++ src/components/Sale/Sale.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+import './Sale.css'
+export default function Sale() {
+    return (
+        <>
+         
+
+            <div className="container">
+                <div className="responsive-row">
+                    <div className="box box-1">
+                        <h4>
+                            Our Offers is good
+                        </h4>
+                        <h2>Big Offer 50%</h2>
+                    </div>
+
+                    <div className="box box-2">
+                    <h4>
+                            Our Offers is good
+                        </h4>
+                        <h2>Big Offer 50%</h2>
+                    </div>
+                    <div className="box box-3">
+                    <h4>
+                            Our Offers is good
+                        </h4>
+                        <h2>Big Offer 50%</h2>
+                    </div>
+                </div>
+            </div>
+
+
+
+        </>
+    )
+}
